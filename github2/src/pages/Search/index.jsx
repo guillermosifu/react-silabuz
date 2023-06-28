4868ada06199375091faadd1aa3c27c7f5444ccb
guillermosifu <sifuentes1201@gmail.com>
2023-06-27 20:04:39 -0500

    github2

---
+++ github2/src/pages/Search/index.jsx
@@ -0,0 +1,20 @@
+import { Button,Container,Grid,TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+
+const Search =()=>{
+
+
+
+    return(
+        <Container>
+            <Grid>
+                <h1>Buscando Usuarios</h1>
+                <TextField/>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Search;
---
+++ github2/src/router/index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Search from "../pages/Search";
+
+
+
+const Router =()=>{
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Search/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
