09b7105d1caae610d90cf3852b572021526b688e
guillermosifu <sifuentes1201@gmail.com>
2023-07-01 10:51:16 -0500

    router

---
+++ login-doc/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Router } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+    <Router/>
+    </>
+  )
+}
+
+export default App
---
+++ login-doc/src/Login/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import{Button,Grid,TextField,Card,CardContent} from"@mui/material"
+
+
+
+
+
+
+const Login = () => {
+  return (
+<h1>Login</h1>
+  )
+}
+
+
+
+export default Login;
